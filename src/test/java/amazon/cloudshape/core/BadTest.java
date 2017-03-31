fb9c18479921663cdaa9bca6ece3de67e05337d6
EC2 Default User <ec2-user@ip-172-31-17-226.eu-west-1.compute.internal>
2017-03-31 06:30:51 +0000

    test2

--- src/test/java/amazon/cloudshape/core/BadTest.java
+++ src/test/java/amazon/cloudshape/core/BadTest.java
@@ -3,12 +3,13 @@ package amazon.cloudshape.core;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class BadTest {
 
     @Test
-    public void meant_to_fail() throws Exception {
+    public void meant_to_fail(){
 	assertEquals(1, 1);
     }
 }
